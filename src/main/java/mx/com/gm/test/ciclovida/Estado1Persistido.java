6186d1c7978965c749b5660734b1d77117777595
LuisFBuelna <luisfbuelna@gmail.com>
2022-09-25 22:57:53 -0600

    Primer commit

---
+++ src/main/java/mx/com/gm/test/ciclovida/Estado1Persistido.java
@@ -0,0 +1,28 @@
+package mx.com.gm.test.ciclovida;
+
+import javax.persistence.*;
+import mx.com.gm.domain.*;
+
+public class Estado1Persistido {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateJpaPU");
+        EntityManager em = emf.createEntityManager();
+        
+        //1. estado transitivo
+        Contacto contacto = new Contacto();
+        contacto.setEmail("clara");
+        contacto.setTelefono("11223344");
+        
+        //2. persistimos el objeto
+        em.getTransaction().begin();
+        
+        em.persist(contacto);
+        em.flush();
+        
+        em.getTransaction().commit();
+        
+        //3. detached (separado)
+        System.out.println("contacto = " + contacto);
+        
+    }
+}
